1873406f3d7eb1a5270b6d0931f53fa266e33d88
jmurphy2404 <murf2073@gmail.com>
2019-05-18 19:00:53 -0400

    second commit

--- main.js
+++ main.js
@@ -1,32 +1,60 @@
 var pdfUtil = require('pdf-to-text');
-var pdf_path = ("./pdf4.pdf");
-var keywords = ['goal', 'sales', 'revenue', 'relationships', 'growth', 'advertising', 'campaign', 'OTT', 'programmatic', 'forecasting', 'digital', 'service']
+var pdf_path = ("./pdf9.pdf");
+var keywords = ['brand partnership',
+'advertising',
+'sales',
+'client relationship',
+'agency relationship',
+'advertiser',
+'pipeline',
+'revenue',
+'goal',
+'RFP',
+'selling',
+'client',
+'digital media',
+'quota',
+'branding',
+'content marketing',
+'online media',
+'programmatic',
+'solution selling']
 
-pdfUtil.pdfToText(pdf_path, function(err, data) {
-  if (err) throw(err);
-  //split data on spaces to get individual words
-  var splitData = data.split(' ');
-  var results = [];
+const results = [];
 
-  //loop through keywords comparing to split resume
-  for(let i=0; i < keywords.length; i++){
-  	for(let j=0; j < splitData.length; j++){
-  		console.log(keywords[i]);
-  		console.log(splitData[j]);
 
-  		//if keyword is found, push it into results
-  		if (keywords[i] == splitData[j]){
-  			results.push(keywords[i]);
-  		} 
-  	}
-  }
+pdfUtil.info(pdf_path, function(err, info){
+	if(info.tagged == "yes"){
+		pdfUtil.pdfToText(pdf_path, function(err, data) {
+		  if (err) throw(err);
+		  //split data on spaces to get individual words
+		  var splitData = data.split(' ');
+		  
 
-  //eliminate duplicates in results
-  for(let k=0; k<results.length; k++){
-  	if (results[k] == results[k+1]){
-  		results.splice(k+1, 1)
-  	}
-  }
+		  //loop through keywords comparing to split resume
+		  for(let i=0; i < keywords.length; i++){
+		  	for(let j=0; j < splitData.length; j++){
+		  		console.log(keywords[i]);
+		  		console.log(splitData[j]);
 
-  console.log(results);
+		  		//if keyword is found, push it into results
+		  		if (keywords[i] == splitData[j]){
+		  			results.push(keywords[i]);
+		  		} 
+		  	}
+		  }
+
+		  //eliminate duplicates in results
+		  for(let k=0; k<results.length; k++){
+		  	for(let l=1; l<results.length-1; l++)
+		  	if (results[k] == results[l]){
+		  		results.splice(l, 1)
+		  	}
+		  }
+
+		  console.log(results);
+		});
+	} else {
+		console.log("manual review");
+	}
 });
